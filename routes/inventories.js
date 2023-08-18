0fa032086e700d83df5b4c8407212d99b6a62125
John Carlo Pierce <johncarlopierce@gmail.com>
2023-08-18 13:39:31 -0400

    first commit

--- routes/inventories.js
+++ routes/inventories.js
@@ -94,4 +94,62 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { warehouse_id, item_name, description, category, status } = req.body;
+
+    const inventory = await knex("inventories")
+      .where({ id })
+      .first();
+        if (!inventory) {
+          return res.status(404).json({
+            error: "Inventory ID not found",
+          });
+        }
+
+    if (
+      !warehouse_id ||
+      !item_name ||
+      !description ||
+      !category ||
+      !status
+    ) {
+      return res.status(400).json({
+        error: "All fields are required!",
+      });
+    }
+
+    const existWarehouse = await knex('warehouses')
+      .where({ id: warehouse_id })
+      .first()
+        if (!existWarehouse) {
+          return res.status(400).json({
+            error: "Warehouse does not exist in the database!",
+          });
+        }
+
+    await knex("inventories")
+      .where({ id })
+      .update({
+        warehouse_id,
+        item_name,
+        description,
+        category,
+        status
+      });
+
+    const updateInventory = await knex("inventories")
+      .where({ id })
+      .first();
+
+    res.status(200).json(updateInventory);
+  } catch(error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 module.exports = router;
